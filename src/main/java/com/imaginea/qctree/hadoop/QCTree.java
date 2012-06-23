5ba905fced1330d4b32eae2d948d3bf804c21502
Kamesh <kam.iitkgp@gmail.com>
2012-06-23 12:39:19 +0530

    adding QCTree.java

---
+++ src/main/java/com/imaginea/qctree/hadoop/QCTree.java
@@ -0,0 +1,21 @@
+package com.imaginea.qctree.hadoop;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
+import org.apache.hadoop.io.Writable;
+
+public class QCTree implements Writable {
+
+	@Override
+	public void readFields(DataInput arg0) throws IOException {
+
+	}
+
+	@Override
+	public void write(DataOutput arg0) throws IOException {
+
+	}
+
+}
