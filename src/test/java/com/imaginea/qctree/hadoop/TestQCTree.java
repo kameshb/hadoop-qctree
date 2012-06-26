67e2a810d124e518fd87017928705365464547b3
Bh V S Kamesh <kam.iitkgp@gmail.com>
2012-06-26 16:57:01 +0530

    modifying test case to reflect the example

--- src/test/java/com/imaginea/qctree/hadoop/TestQCTree.java
+++ src/test/java/com/imaginea/qctree/hadoop/TestQCTree.java
@@ -26,15 +26,31 @@ public class TestQCTree {
     root.setUpperBound(new Cell(new String[] { "*", "*", "*" }));
     root.setLowerBound(new Cell(new String[] { "*", "*", "*" }));
     root.setChildID(-1);
-    root.setClassID(0);
+    root.setClassID(1);
     root.setAggregate(9);
     tree = new QCTree(root);
-
+    
     Class clazz = new Class();
-    clazz.setUpperBound(new Cell(new String[] { "*", "P1", "*" }));
+    clazz.setUpperBound(new Cell(new String[] { "S1", "P2", "s" }));
+    clazz.setLowerBound(new Cell(new String[] { "*", "P2", "*" }));
+    clazz.setChildID(5);
+    clazz.setClassID(2);
+    clazz.setAggregate(12);
+    tree.add(clazz);
+    
+    clazz = new Class();
+    clazz.setUpperBound(new Cell(new String[] { "S2", "P1", "f" }));
     clazz.setLowerBound(new Cell(new String[] { "*", "P1", "*" }));
     clazz.setChildID(5);
-    clazz.setClassID(6);
+    clazz.setClassID(3);
+    clazz.setAggregate(7.5);
+    tree.add(clazz);
+    
+    clazz = new Class();
+    clazz.setUpperBound(new Cell(new String[] { "S1", "*", "s" }));
+    clazz.setLowerBound(new Cell(new String[] { "*", "*", "s" }));
+    clazz.setChildID(5);
+    clazz.setClassID(4);
     clazz.setAggregate(7.5);
     tree.add(clazz);
 
@@ -42,43 +58,41 @@ public class TestQCTree {
     clazz.setUpperBound(new Cell(new String[] { "S1", "P1", "s" }));
     clazz.setLowerBound(new Cell(new String[] { "*", "P1", "s" }));
     clazz.setChildID(5);
-    clazz.setClassID(6);
+    clazz.setClassID(5);
     clazz.setAggregate(6);
     tree.add(clazz);
-
+    
     clazz = new Class();
-    clazz.setUpperBound(new Cell(new String[] { "S1", "P2", "s" }));
-    clazz.setLowerBound(new Cell(new String[] { "*", "P2", "*" }));
-    clazz.setChildID(5);
-    clazz.setClassID(6);
-    clazz.setAggregate(12);
-    tree.add(clazz);
-
-    clazz = new Class();
-    clazz.setUpperBound(new Cell(new String[] { "S1", "P1", "t" }));
+    clazz.setUpperBound(new Cell(new String[] { "*", "P1", "*" }));
     clazz.setLowerBound(new Cell(new String[] { "*", "P1", "*" }));
     clazz.setChildID(5);
     clazz.setClassID(6);
-    clazz.setAggregate(12);
+    clazz.setAggregate(7.5);
     tree.add(clazz);
+
   }
 
   @Test
   public void testShouldConstructQCTree() throws IOException {
     StringBuilder sb = new StringBuilder();
     sb.append("ALL = * : 9.0 true").append('\n');
-    sb.append("dim2 = P1 : 7.5 false").append('\n');
-    sb.append("dim1 = S1 : 6.0 true").append('\n');
-    sb.append("dim2 = P1 : 6.0 true").append('\n');
-    sb.append("dim3 = s : 6.0 false").append('\n');
-    sb.append("dim3 = t : 12.0 false").append('\n');
-    sb.append("NONE = NONE : 0.0").append('\n');
+    sb.append("dim1 = S1 : 12.0 true").append('\n');
     sb.append("dim2 = P2 : 12.0 true").append('\n');
     sb.append("dim3 = s : 12.0 false").append('\n');
     sb.append("NONE = NONE : 0.0").append('\n');
+    sb.append("dim3 = s : 7.5 false").append('\n');
+    sb.append("dim2 = P1 : 6.0 true").append('\n');
+    sb.append("dim3 = s : 6.0 false").append('\n');
     sb.append("NONE = NONE : 0.0").append('\n');
     sb.append("NONE = NONE : 0.0").append('\n');
-
+    sb.append("dim1 = S2 : 7.5 true").append('\n');
+    sb.append("dim2 = P1 : 7.5 true").append('\n');
+    sb.append("dim3 = f : 7.5 false").append('\n');
+    sb.append("NONE = NONE : 0.0").append('\n');
+    sb.append("NONE = NONE : 0.0").append('\n');
+    sb.append("dim2 = P1 : 7.5 false").append('\n');
+    sb.append("NONE = NONE : 0.0").append('\n');
+    
     Assert.assertEquals(tree.toString(), sb.toString());
   }
 
