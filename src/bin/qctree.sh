a09f90add35d9129f94269e99e07914f679dc79f
Bh V S Kamesh <kam.iitkgp@gmail.com>
2012-06-28 16:59:22 +0530

    adding script for execution

---
+++ src/bin/qctree.sh
@@ -0,0 +1,28 @@
+bin=`dirname "${BASH_SOURCE-$0}"`
+bin=`cd "$bin"; pwd`
+
+if [ $# = 0 ]; then
+  echo "Usage: qctree input output"
+  exit 1
+fi
+
+if [ "$JAVA_HOME" != "" ]; then
+  JAVA_HOME=$JAVA_HOME
+fi
+  
+if [ "$JAVA_HOME" = "" ]; then
+  echo "Error: JAVA_HOME is not set."
+  exit 1
+fi
+
+JAVA=$JAVA_HOME/bin/java
+
+QCT_HOME="$bin"/..
+
+CLASSPATH="${QCT_HOME}/conf"
+CLASSPATH=${CLASSPATH}:"$QCT_HOME/lib/*"
+
+CLASS='com.imaginea.qctree.hadoop.QCDriver'
+
+exec "$JAVA" -classpath "$CLASSPATH" $CLASS "$@"
+
